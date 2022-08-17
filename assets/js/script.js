3f0fe4182ce84c6365772d04c8d68ae9e149aa32
Gabe Vee <0gabevee0@gmail.com>
2022-08-17 11:12:55 -0600

    added function to call food ingredient api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,38 @@
 ///////////////////////////////      Variable Declarations      ////////////////////////
 // Get elements with jQuery
-// search Button 
+// search Button
 var searchBtn = $("#button");
 // text area input for search
 var searchInput = $(".form-control");
 
-
-
 ////////////////////////////////      Functions        ///////////////////////////////
+function searchIngredient(data) {
+  var API_KEY = "f821a94c134c496c176e82b63b0ade69";
+  var searchresults = data; //data
+  var nutritionUrl =
+    "https://api.edamam.com/api/nutrition-data?app_id=ddbb3418&app_key=" +
+    API_KEY +
+    "&nutrition-type=logging&ingr=" +
+    searchresults;
 
-
-
-
-
-
-
+  fetch(nutritionUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.totalWeight); //grams
+      console.log(data.calories); //cal
+      console.log(data.totalNutrients.FAT.quantity); //grams
+      console.log(data.totalNutrients.PROCNT.quantity); //grams
+      console.log(data.totalNutrients.CHOCDF.quantity); //grams
+      console.log(data.totalNutrients.SUGAR.quantity); //grams
+      console.log(data);
+    })
+    .catch(function (e) {
+      console.log(e);
+    });
+}
+searchIngredient("/,.,//2324234");
 /////////////////////////////       Event handlers      ///////////////////////////
-
-
-
-
-
-
 
 //////////////////////////     Execute at lauch functions        ///////////////////////////////
