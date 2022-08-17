72f8c405a94b0ee7d59d1ae7ab2f3291db6aa565
Hallie <halliemestas@gmail.com>
2022-08-17 13:04:49 -0600

    photo API functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,63 +4,87 @@
 var searchBtn = $("#button");
 // text area input for search
 var searchInput = $(".form-control");
+// Nutritional information
+var nutriData = $("li");
+// Get element for name of ingredient
+var ingrName = $("#search-name");
+
 var imgCard = $(".card-img");
 
+
 ////////////////////////////////      Functions        ///////////////////////////////
-function searchIngredient(data) {
-  var API_KEY = "f821a94c134c496c176e82b63b0ade69";
-  var searchresults = data; //data
-  var nutritionUrl =
-    "https://api.edamam.com/api/nutrition-data?app_id=ddbb3418&app_key=" +
-    API_KEY +
-    "&nutrition-type=logging&ingr=" +
-    searchresults;
-
-  fetch(nutritionUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data.totalWeight); //grams
-      console.log(data.calories); //cal
-      console.log(data.totalNutrients.FAT.quantity); //grams
-      console.log(data.totalNutrients.PROCNT.quantity); //grams
-      console.log(data.totalNutrients.CHOCDF.quantity); //grams
-      console.log(data.totalNutrients.SUGAR.quantity); //grams
-      console.log(data);
-    })
-    .catch(function (e) {
-      console.log(e);
-    });
-}
-searchIngredient("/,.,//2324234");
-
-
-function getPhoto(input)
-{
-    var api_key = "563492ad6f917000010000010e9c974a628f412faac80ae718c8039d";
-    const query = input;
-
-    const settings = {
-        url:"https://api.pexels.com/v1/search?query=" + query + "&per_page=1",
-        method:"GET",
-        headers:
-        {
-            "AUTHORIZATION":api_key
+function searchIngredient(ingredient) {
+    var API_KEY = "f821a94c134c496c176e82b63b0ade69";
+    // This a required parameter need to pass into the function, from search input -ingredient 
+    var searchresults = ingredient; 
+    var nutritionUrl =
+      "https://api.edamam.com/api/nutrition-data?app_id=ddbb3418&app_key=" +
+      API_KEY +
+      "&nutrition-type=logging&ingr=" +
+      searchresults;
+    fetch(nutritionUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // get data for api and put into DOM
+        console.log(data);
+        // Create array to store nutrient data
+        var nutriArray = [
+            `Quanity: ${data.totalWeight} grams`,
+            `Calories: ${data.calories} cal`,
+            `Fat: ${data.totalNutrients.FAT.quantity} grams`,
+            `Protein: ${data.totalNutrients.PROCNT.quantity} grams`,
+            `Carbs: ${data.totalNutrients.CHOCDF.quantity} grams`,
+            `Sugar: ${data.totalNutrients.SUGAR.quantity} grams`,
+        ];
+        
+        // Loop through li elements and put in values from api call
+        for (var i =0; i<nutriArray.length;i++){
+            nutriData.eq(i).text(nutriArray[i]);
         }
-    }
 
-    $.ajax(settings).then(function(response)
-    {
-        var obj = response.photos[0].src.original;
-        imgCard.attr("src", obj);
-        console.log(obj);
-        console.log(response)
-    })
-    
-}
+        // Add ingredient name to dom 
+        ingrName.text(ingredient.toUpperCase())
+      })
+    //   catch any errors from api call, just console.log for now
+      .catch(function (e) {
+        console.log(e);
+      });
+  }
+//   Only for testing purposes 
+  searchIngredient("almonds");
 
-getPhoto("broccoli");
+  function getPhoto(input)
+  {
+      var api_key = "563492ad6f917000010000010e9c974a628f412faac80ae718c8039d";
+      const query = input;
+  
+      const settings = {
+          url:"https://api.pexels.com/v1/search?query=" + query + "&per_page=1",
+          method:"GET",
+          headers:
+          {
+              "AUTHORIZATION":api_key
+          }
+      }
+  
+      $.ajax(settings).then(function(response)
+      {
+          var obj = response.photos[0].src.original;
+          imgCard.attr("src", obj);
+          console.log(obj);
+          console.log(response)
+      })
+      
+  }
+  
+  //only for testing purposes
+  getPhoto("broccoli");
+
+
+
+
 /////////////////////////////       Event handlers      ///////////////////////////
 
 //////////////////////////     Execute at lauch functions        ///////////////////////////////
