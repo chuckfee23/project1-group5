30dab094865d68c3c28169068b9a46d06363ecc7
myang5t3r <matthew.yang@hunterdouglas.com>
2022-08-17 12:10:20 -0600

    Finished added values from API to dom elements, Name of ingredient and li listgit add assets/js/script.js !

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,16 @@ var searchBtn = $("#button");
 // text area input for search
 var searchInput = $(".form-control");
 // Nutritional information
-var nutriData = $("li")
+var nutriData = $("li");
+// Get element for name of ingredient
+var ingrName = $("#search-name");
 
 
 ////////////////////////////////      Functions        ///////////////////////////////
-function searchIngredient(data) {
+function searchIngredient(ingredient) {
     var API_KEY = "f821a94c134c496c176e82b63b0ade69";
-    var searchresults = data; //data
+    // This a required parameter need to pass into the function, from search input -ingredient 
+    var searchresults = ingredient; 
     var nutritionUrl =
       "https://api.edamam.com/api/nutrition-data?app_id=ddbb3418&app_key=" +
       API_KEY +
@@ -22,25 +25,32 @@ function searchIngredient(data) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data.totalWeight); //grams
-        console.log(data.calories); //cal
-        console.log(data.totalNutrients.FAT.quantity); //grams
-        console.log(data.totalNutrients.PROCNT.quantity); //grams
-        console.log(data.totalNutrients.CHOCDF.quantity); //grams
-        console.log(data.totalNutrients.SUGAR.quantity); //grams
-        console.log(data);
         // get data for api and put into DOM
-        console.log(nutriData);
-        // create array to store nutrient data
+        console.log(data);
+        // Create array to store nutrient data
         var nutriArray = [
             `Quanity: ${data.totalWeight} grams`,
-            `Calories `
-        ]
+            `Calories: ${data.calories} cal`,
+            `Fat: ${data.totalNutrients.FAT.quantity} grams`,
+            `Protein: ${data.totalNutrients.PROCNT.quantity} grams`,
+            `Carbs: ${data.totalNutrients.CHOCDF.quantity} grams`,
+            `Sugar: ${data.totalNutrients.SUGAR.quantity} grams`,
+        ];
+        
+        // Loop through li elements and put in values from api call
+        for (var i =0; i<nutriArray.length;i++){
+            nutriData.eq(i).text(nutriArray[i]);
+        }
+
+        // Add ingredient name to dom 
+        ingrName.text(ingredient.toUpperCase())
       })
+    //   catch any errors from api call, just console.log for now
       .catch(function (e) {
         console.log(e);
       });
   }
+//   Only for testing purposes 
   searchIngredient("almonds");
 
 
